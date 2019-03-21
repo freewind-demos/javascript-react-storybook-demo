bd785baef3226803fb6cbbb176c22e0c2add8776
freewind <nowindlee@gmail.com>
2019-03-21 13:34:39 +0800

    fix

--- stories/hello.stories.js
+++ stories/hello.stories.js
@@ -2,5 +2,5 @@ import React from 'react';
 import {storiesOf} from '@storybook/react';
 import Hello from '../hello.jsx';
 
-storiesOf('Hello')
+storiesOf('Hello', module)
   .add('just hello', () => <Hello/>);
